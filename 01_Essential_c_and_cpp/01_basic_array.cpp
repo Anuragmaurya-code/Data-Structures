3e1982cf1f5117344af9b419844880b2a95bd1be
Anurag Maurya <mauryaanurag375@gmail.com>
2021-11-26 00:11:57 +0530

    Basics of structure and array

--- 01_Essential_c_and_cpp/01_basic_array.cpp
+++ 01_Essential_c_and_cpp/01_basic_array.cpp
@@ -2,5 +2,11 @@
 using namespace std;
 int main()
 {
-    cout<<"Anurag : Hello";
+    int a[5]={1,2,3,2,1};
+    for(int x:a)
+    {
+        x=x*2;
+        cout<<x<<endl;//Length of array as each int is of 4bit therefore 4*5=20bit lenth or size 
+    }
+     
 }
---
+++ 01_Essential_c_and_cpp/02_structure.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+struct reportcard
+{
+    char name[20];
+    int roll;
+    int tot_marks;
+
+};
+
+int main()
+{
+    int i;
+    struct reportcard r[3];
+    for(i=0;i<3;i++)
+    {
+        cout<<"Name ";
+        cin>>r[i].name;
+        cout<<"roll no ";
+        cin>>r[i].roll;
+        cout<<"Total Marks ";
+        cin>>r[i].tot_marks;
+        cout<<endl;
+    }
+    for(i=0;i<3;i++)
+    {
+        cout<<r[i].name<<" "<< r[i].roll << " "<< r[i].tot_marks << endl;
+    }
+
+    return 0;
+}
