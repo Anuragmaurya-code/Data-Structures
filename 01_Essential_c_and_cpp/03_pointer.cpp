f852446eaf7834b228f4616878e9c6b5447d6345
Anurag Maurya <mauryaanurag375@gmail.com>
2021-11-30 09:02:13 +0530

    30/11/2021

---
+++ 01_Essential_c_and_cpp/03_pointer.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int a[5]={1,3,5,7,9};
+    int *p=&a[0],*p2,*p3;
+    printf("Stack Memory array %d\n",p[2]);//using pointer p as array a
+    p2=(int *)malloc(5*sizeof(int));//accessing heap memory using pointer in c
+    p3=new int(5);//accessing heap memory using pointer in c++
+    p2[0]=4;
+    p3[0]=5;
+    printf("Heap Memory array %d %d \n",p2[0],p3[0]);
+    free(p2);//dealocating heap memory in c
+    delete [ ] p3;//dealocating heap memory in c++
+    printf("Heap Memory array %d %d ",p2[0],p3[0]);
+    return 0;
+}
